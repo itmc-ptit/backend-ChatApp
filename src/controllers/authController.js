5e03d6ae4cbbf6245aef80f8bdb9881361243df9
Tuan Nhat <letuannhat105@gmail.com>
2024-03-15 16:17:36 +0700

    valid email

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,16 @@
 import { generateToken } from "~/services/jwt";
 import User from "../models/userModel";
 import { hash } from "bcrypt";
+import { validateEmail } from "~/utils/validEmail";
 
 const register = async (req, res, next) => {
   try {
     const { name, avatar, email, password } = req.body;
+    if (!validateEmail(email)) {
+      const error = new Error("Invalid email");
+      error.statusCode = 401;
+      return next(error);
+    }
     let user = await User.findOne({ email });
     if (user.length !== 0) {
       return res.status(400).json({ message: "Email already exists" });
